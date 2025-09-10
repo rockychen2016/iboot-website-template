e71ba7e676b990e6bb83474812d478eeda6b8305
rock.chen <szygnet@qq.com>
2025-09-10 13:33:17 +0800

    UPDATE

--- scripts/create-env-files.js
+++ scripts/create-env-files.js
@@ -7,6 +7,10 @@ envFiles.forEach(file => {
   const filePath = path.join(process.cwd(), file);
   console.log('start copy file >>>', filePath)
   if (!fs.existsSync(filePath)) {
-    fs.copyFileSync(path.join(__dirname, '..', `${file}.example`), filePath);
+    const envFile = path.join(__dirname, '..', `${file}.example`);
+    fs.copyFileSync(envFile, filePath);
+    // 删除 .example 文件
+    fs.unlinkSync(envFile);
+    console.log('deleted example file >>>', envFile);
   }
 });
