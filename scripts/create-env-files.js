bb11bd08c284c3c756f702dce966cbea99b0a095
rock.chen <szygnet@qq.com>
2025-09-10 12:12:18 +0800

    UPDATE

--- scripts/create-env-files.js
+++ scripts/create-env-files.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
-const path = require('path');
-
-const envFiles = ['.env.local', '.env.production', '.env.test'];
+//const fs = require('fs');
+//const path = require('path');
+import { fs } from 'fs';
+import { path } from 'path';
+const envFiles = ['.env.development', '.env.local', '.env.production'];
 
 envFiles.forEach(file => {
   const filePath = path.join(process.cwd(), file);
+  console.log('start copy file >>>', filePath)
   if (!fs.existsSync(filePath)) {
     fs.copyFileSync(path.join(__dirname, '..', `${file}.example`), filePath);
   }
